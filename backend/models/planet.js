fec4c08f01e7ec8889c5cbb1efececd1d8de71b2
Aleksandra <baliaks1@stu.vistula.edu.pl>
2025-10-17 20:30:13 +0300

    Add diameter and orbital_period to Planet model

--- backend/models/planet.js
+++ backend/models/planet.js
@@ -1,22 +1,12 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../db');
-
-const Planet = sequelize.define('Planet', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  description: {
-    type: DataTypes.TEXT
-  },
-  diameter: {
-    type: DataTypes.FLOAT,
-    allowNull: true
-  },
-  orbitalPeriod: {
-    type: DataTypes.FLOAT,
-    allowNull: true
-  }
-});
+const Planet = {
+  id: Number,
+  name: String,
+  system: String,
+  climate: String,
+  population: Number,
+  surface_type: String,
+  diameter: Number,
+  orbital_period: Number
+};
 
 module.exports = Planet;
