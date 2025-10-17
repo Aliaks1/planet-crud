a6cbdf879793fb76888ae3ef44f1672995d2a619
Aleksandra <baliaks1@stu.vistula.edu.pl>
2025-10-17 20:01:40 +0300

    Add diameter and orbitalPeriod fields to Planet model

--- backend/models/planet.js
+++ backend/models/planet.js
@@ -1,10 +1,22 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize({ dialect: 'sqlite', storage: './planets.db' });
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
 const Planet = sequelize.define('Planet', {
-  name: { type: DataTypes.STRING, allowNull: false },
-  system: { type: DataTypes.STRING, allowNull: false },
-  climate: { type: DataTypes.STRING, allowNull: false },
-  population: { type: DataTypes.INTEGER, allowNull: false }
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT
+  },
+  diameter: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  orbitalPeriod: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  }
 });
-sequelize.sync();
+
 module.exports = Planet;
